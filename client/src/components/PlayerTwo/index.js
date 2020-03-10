e22525c72f6b373e08e0878c94deec4a9c5faa26
Jeffrey Poe <jeffreypoe@jeffreys-mbp-2.lan>
2020-03-09 21:05:18 -0400

    Added characters to roster

--- client/src/components/PlayerTwo/index.js
+++ client/src/components/PlayerTwo/index.js
@@ -16,33 +16,71 @@ class PlayerTwo extends React.Component {
   }
   render() {
     return (
-      <div id="carouselOne">
+      <div id="carouselTwo">
         <div className="carousel">
-          <a className="carousel-item" href="#one!">
-            <img src="https://vignette.wikia.nocookie.net/witcher/images/f/f3/Tw3_journal_mirrorman.png/revision/latest/scale-to-width-down/350?cb=20170501180556" />
+          <a className="carousel-item" href="#one!" id="odimmTwo">
+            <img
+              src="https://vignette.wikia.nocookie.net/witcher/images/f/f3/Tw3_journal_mirrorman.png/revision/latest/scale-to-width-down/350?cb=20170501180556"
+              alt="Gaunter O Dimm"
+            />
           </a>
-          <a className="carousel-item" href="#two!">
-            <img src="https://vignette.wikia.nocookie.net/bokunoheroacademia/images/2/22/Endeavor_Anime_Profile.png/revision/latest/scale-to-width-down/239?cb=20170906213725" />
+          <a className="carousel-item" href="#two!" id="endeavorTwo">
+            <img
+              src="https://vignette.wikia.nocookie.net/bokunoheroacademia/images/2/22/Endeavor_Anime_Profile.png/revision/latest/scale-to-width-down/239?cb=20170906213725"
+              alt="Endeavor"
+            />
           </a>
-          <a className="carousel-item" href="#three!">
-            <img src="https://vignette.wikia.nocookie.net/bokunoheroacademia/images/5/55/All_Might_Hero_Form_Full_Body.png/revision/latest/scale-to-width-down/311?cb=20190129015644" />
+          <a className="carousel-item" href="#three!" id="allmightTwo">
+            <img
+              src="https://vignette.wikia.nocookie.net/bokunoheroacademia/images/5/55/All_Might_Hero_Form_Full_Body.png/revision/latest/scale-to-width-down/311?cb=20190129015644"
+              alt="Allmight"
+            />
           </a>
-          <a className="carousel-item" href="#four!">
-            <img src="https://vignette.wikia.nocookie.net/bokunoheroacademia/images/f/f7/Izuku_Midoriya_Hero_Costume_Action_Pose.png/revision/latest/scale-to-width-down/350?cb=20190909030259" />
+          <a className="carousel-item" href="#four!" id="dekuTwo">
+            <img
+              src="https://vignette.wikia.nocookie.net/bokunoheroacademia/images/f/f7/Izuku_Midoriya_Hero_Costume_Action_Pose.png/revision/latest/scale-to-width-down/350?cb=20190909030259"
+              alt="Deku"
+            />
           </a>
-          <a className="carousel-item" href="#five!">
-            <img src="https://vignette.wikia.nocookie.net/zelda/images/5/57/Breath_of_the_Wild_Artwork_Link_%28Official_Artwork%29.png/revision/latest/scale-to-width-down/350?cb=20160623185226" />
+          <a className="carousel-item" href="#five!" id="linkTwo">
+            <img
+              src="https://vignette.wikia.nocookie.net/zelda/images/5/57/Breath_of_the_Wild_Artwork_Link_%28Official_Artwork%29.png/revision/latest/scale-to-width-down/350?cb=20160623185226"
+              alt="Link"
+            />
           </a>
-          <a className="carousel-item" href="#six!">
-            <img src="https://vignette.wikia.nocookie.net/p__/images/2/22/Dragon-Ball-Goku-PNG-Free-Download.png/revision/latest/scale-to-width-down/347?cb=20181207162901&path-prefix=protagonist" />
+          <a className="carousel-item" href="#six!" id="gokuTwo">
+            <img
+              src="https://vignette.wikia.nocookie.net/p__/images/2/22/Dragon-Ball-Goku-PNG-Free-Download.png/revision/latest/scale-to-width-down/347?cb=20181207162901&path-prefix=protagonist"
+              alt="Goku"
+            />
           </a>
-          <a className="carousel-item" href="#seven!">
-            <img src="https://vignette.wikia.nocookie.net/mario/images/6/62/840px-SuperMarioParty_Waluigi.png/revision/latest/scale-to-width-down/310?cb=20190519205517" />
+          <a className="carousel-item" href="#seven!" id="waluigiTwo">
+            <img
+              src="https://vignette.wikia.nocookie.net/mario/images/6/62/840px-SuperMarioParty_Waluigi.png/revision/latest/scale-to-width-down/310?cb=20190519205517"
+              alt="Waluigi"
+            />
+          </a>
+          <a className="carousel-item" href="#eight!" id="allForOneTwo">
+            <img
+              src="https://vignette.wikia.nocookie.net/bokunoheroacademia/images/3/32/All_For_One_Profile.png/revision/latest/scale-to-width-down/243?cb=20180528014920"
+              alt="All For One"
+            />
+          </a>
+          <a className="carousel-item" href="#nine!" id="inteleonTwo">
+            <img
+              src="https://cdn.bulbagarden.net/upload/thumb/e/e4/818Inteleon.png/250px-818Inteleon.png"
+              alt="Intellion"
+            />
+          </a>
+          <a className="carousel-item" href="#ten!" id="trissTwo">
+            <img
+              src="https://vignette.wikia.nocookie.net/witcher/images/2/27/Triss-TW3-new-render.png/revision/latest/scale-to-width-down/313?cb=20160402173701"
+              alt="Triss"
+            />
           </a>
         </div>
       </div>
     );
   }
 }
-
 export default PlayerTwo;
